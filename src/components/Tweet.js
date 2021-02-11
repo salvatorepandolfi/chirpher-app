7a962f7ffec287b9b6b0b39203071c315feec6ea
Salvatore Pandolfi <salvpan@gmail.com>
2021-02-11 22:53:36 +0100

    Added parent on Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,9 +4,13 @@ import {formatTweet} from '../utils/helpers'
 
 class Tweet extends Component {
     render() {
-        console.log(this.props.tweet)
+        const {tweet} = this.props
+        if(tweet === null) {
+            return <p>This Tweet doesn't exists</p>
+        }
+        console.log(this.props)
         return (
-            <div  className="tweet">
+            <div className="tweet">
                 {this.props.id}
             </div>
         )
@@ -16,9 +20,11 @@ class Tweet extends Component {
 //the second argument is the prop passed to the component that we want to use in our map function
 function mapStateToProps({tweets, authedUser, users}, {id}) {
     const tweet = tweets[id]
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
+
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
     }
 
 }
