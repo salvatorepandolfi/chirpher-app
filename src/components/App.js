7eaaa8da44ba01c42e2ad2093f709b8ec56ba900
Salvatore Pandolfi <salvpan@gmail.com>
2021-02-11 22:35:11 +0100

    Added Dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
+import Dashboard from "./Dashboard";
 
 class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(handleInitialData())
-  }
+    componentDidMount() {
+        this.props.dispatch(handleInitialData())
+    }
 
-  render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                {this.props.loading === true
+                    ? null
+                    : <Dashboard/>
+                }
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({authedUser}) {
+    return {
+        loading: authedUser === null
+    }
 }
 
 export default connect()(App)
